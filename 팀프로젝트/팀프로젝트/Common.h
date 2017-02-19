63de5c9b118504459cdb88974216acf09c03415c
wisd2012@gmail.com <a8482469>
2017-02-19 18:42:38 +0900

    아싸

--- 팀프로젝트/팀프로젝트/Common.h
+++ 팀프로젝트/팀프로젝트/Common.h
@@ -141,13 +141,13 @@ public:
 	{
 		switch (name)
 		{
-		case FIRE_BOLT:			SetSkillStatus(0.001f, 100, 3, 1, ATTRIBUTE_FIRE, TYPE_BOLT);					break;
+		case FIRE_BOLT:			SetSkillStatus(0.001f, 1000, 3, 1, ATTRIBUTE_FIRE, TYPE_BOLT);					break;
 		case FIRE_WALL:			SetSkillStatus(0.001f,0, 4, 1000, ATTRIBUTE_FIRE, TYPE_WALL);				break;
 		case FIRE_SHIELD:		SetSkillStatus(0.001f, 0, 5, 500, ATTRIBUTE_FIRE, TYPE_SHIELD);				break;
-		case WATER_BOLT:		SetSkillStatus(0.001f, 100, 3, 1, ATTRIBUTE_WATER, TYPE_BOLT);				break;
+		case WATER_BOLT:		SetSkillStatus(0.001f, 1000, 3, 1, ATTRIBUTE_WATER, TYPE_BOLT);				break;
 		case WATER_WALL:		SetSkillStatus(0.001f, 0, 4, 1000, ATTRIBUTE_WATER, TYPE_WALL);				break;
 		case WATER_SHIELD:		SetSkillStatus(0.001f,0, 5, 500, ATTRIBUTE_WATER, TYPE_SHIELD);				break;
-		case ELECTRICITY_BOLT:	SetSkillStatus(0.001f, 100,3, 1, ATTRIBUTE_ELECTRICITY, TYPE_BOLT);			break;
+		case ELECTRICITY_BOLT:	SetSkillStatus(0.001f, 1000,3, 1, ATTRIBUTE_ELECTRICITY, TYPE_BOLT);			break;
 		case ELECTRICITY_WALL:	SetSkillStatus(0.001f, 0, 4, 1000, ATTRIBUTE_ELECTRICITY, TYPE_WALL);		break;
 		case ELECTRICITY_SHIELD:SetSkillStatus(0.001f,0, 5, 500, ATTRIBUTE_ELECTRICITY, TYPE_SHIELD);			break;
 		}
